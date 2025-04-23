72e0987c41933b6ef598de84f5c3eb338c35e704
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-04-23 23:35:24 +0600

    Adds banner section with intro text
    Adds a banner section with introductory text and styling.
    
    Defines a container utility class in global styles for consistent layout.

--- src/components/sections/banner/index.tsx
+++ src/components/sections/banner/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const BannerSection = () => {
-  return <div>BannarSection</div>;
+  return (
+    <div className="h-screen w-full flex flex-col justify-center">
+      <div className="container mx-auto">
+        <h1 className="text-7xl">
+          <span className="text-blue-600">Frontend </span> <br />
+          <span>Developer</span>
+        </h1>
+        <p>
+         {" Hi! I'm Sakil Anwar. A creative Frontend Developer with 3+ years of  experience in building high-performance, scalable, and responsive web solutions."}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BannerSection;
