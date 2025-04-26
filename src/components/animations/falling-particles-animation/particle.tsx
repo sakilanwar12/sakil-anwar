4044cbe38c911e079d75c33acc04c1fb10b63955
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-04-26 20:59:36 +0600

    Optimizes particle initialization using useMemo
    Refactors particle initialization logic to use `useMemo`.
    
    This improves performance by ensuring that particle properties
    (startX, opacity, size, fallDuration) are only calculated once
    when the component mounts, rather than on every render.

--- src/components/animations/falling-particles-animation/particle.tsx
+++ src/components/animations/falling-particles-animation/particle.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 const Particle = () => {
-  const [startX, setStartX] = useState(0);
-  const [opacity, setOpacity] = useState(0.6);
-  const [size, setSize] = useState(3);
-  const [fallDuration, setFallDuration] = useState(0);
-
-  useEffect(() => {
-    setStartX(Math.random() * 100);
-    setOpacity(0.3 + Math.random() * 0.7);
-    setSize(2 + Math.random() * 4);
-    setFallDuration(10 + Math.random() * 5);
+  const { startX, opacity, size, fallDuration } = useMemo(() => {
+    return {
+      startX: Math.random() * 100,
+      opacity: 0.3 + Math.random() * 0.7,
+      size: 2 + Math.random() * 4,
+      fallDuration: 10 + Math.random() * 5,
+    };
   }, []);
 
   return (
     <div
-      className="absolute -top-1 size-1 rounded-full bg-gray-400 drop-shadow   animate-fall"
+      className="absolute -top-1 rounded-full bg-gray-400 drop-shadow animate-fall"
       style={{
         left: `${startX}%`,
         opacity,
