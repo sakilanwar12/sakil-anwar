a433d595bd9b1d3f3a2e381ac358bcd74754e536
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-04-26 21:43:24 +0600

    Adds falling particles animation
    Improves user experience by adding a subtle falling particles animation to the background.
    
    Moves the animation component to layout to ensure it persists across page transitions and adds smooth scrolling behavior to the page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import FallingParticlesAnimation from "@/components/animations/falling-particles-animation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-slate-100`}
       >
+              <FallingParticlesAnimation />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-import FallingParticlesAnimation from "@/components/animations/falling-particles-animation";
 import BannerSection from "@/components/sections/banner";
 
 export default function Home() {
   return (
     <div>
-      <FallingParticlesAnimation />
       <BannerSection />
       <BannerSection />
       <BannerSection />
