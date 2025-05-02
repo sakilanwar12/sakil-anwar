686f29bb6de7d161ba47bed2f01f517127b254fc
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-05-02 20:28:51 +0600

    add seo meta

---
+++ src/lib/utils/get-seo-meta.ts
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+
+export type TGetSeoMetaArgs = Metadata | void;
+export const getSeoMeta = (props: TGetSeoMetaArgs): Metadata => {
+  const {
+    title = "Portfolio - Sakil Anwar",
+    applicationName = "Portfolio - Sakil Anwar",
+    description,
+    manifest = "/assets/fav-icon/site.webmanifest",
+    icons = {
+      icon: "/assets/fav-icon/favicon-32x32.png",
+      apple: "/assets/fav-icon/apple-touch-icon.png",
+    },
+    ...restProps
+  } = props || {};
+  return {
+    title,
+    applicationName,
+    description,
+    manifest,
+    icons,
+    ...restProps,
+  };
+};
