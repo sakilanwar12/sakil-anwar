aaa51ba0354774b2bf65a9f8cf6e04930c6f91f4
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-05-02 21:23:30 +0600

    add bannar

--- src/components/sections/banner/index.tsx
+++ src/components/sections/banner/index.tsx
@@ -3,10 +3,10 @@ const BannerSection = () => {
     <div className="h-screen w-full flex flex-col justify-center">
       <div className="container mx-auto">
         <h1 className="text-7xl">
-          <span className="text-blue-600">Frontend </span> <br />
+          <span className="text-primary">Frontend </span> <br />
           <span>Developer</span>
         </h1>
-        <p>
+        <p className="max-w-[400px]">
          {" Hi! I'm Sakil Anwar. A creative Frontend Developer with 3+ years of  experience in building high-performance, scalable, and responsive web solutions."}
         </p>
         
