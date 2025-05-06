3e469640400f0eca263223aba62752b1fc30313e
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-05-07 00:03:44 +0600

    add about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
+import AboutMe from "@/components/sections/about-me";
 import BannerSection from "@/components/sections/banner";
 
 export default function Home() {
   return (
     <div>
       <BannerSection />
-      <BannerSection />
-      <BannerSection />
-      <BannerSection />
-      <BannerSection />
-      <BannerSection />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/sections/about-me.tsx
@@ -0,0 +1,34 @@
+import { Separator } from "@/components/ui/separator";
+
+const AboutMe = () => {
+  return (
+    <section>
+      <div className="container">
+        <p className="text-[60px] leading-[70px]">
+          I believe in a user centered design approach, ensuring that every
+          project I work on is tailored to meet the specific needs of its users.
+        </p>
+        <p>This is me</p>
+        <Separator />
+        <div className="grid grid-cols-12 gap-6">
+          <div className="col-span-12 lg:col-span-4">
+            <h2>Hi, I'm Sakil Anwar.</h2>
+          </div>
+          <div className="col-span-12 lg:col-span-8">
+            <p>
+              I'm a frontend web developer dedicated to turning ideas into
+              creative solutions. I specialize in creating seamless and
+              intuitive user experiences. My approach focuses on creating
+              scalable, high-performing solutions tailored to both user needs
+              and business objectives. By prioritizing performance,
+              accessibility, and responsiveness, I strive to deliver experiences
+              that not only engage users but also drive tangible results.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
