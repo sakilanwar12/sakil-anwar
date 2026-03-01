9ee8d53d18f1bb4becf1551612345b18412844d1
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2026-03-01 22:35:02 +0600

    add project card

--- src/app/Components/MyWork.tsx
+++ src/app/Components/MyWork.tsx
@@ -2,7 +2,7 @@ import ProjectCardList from "@/components/sections/ProjectCardList";
 
 function MyWork() {
   return (
-    <div className="mt-20">
+    <div className="my-20">
       <h3 className="text-default-100 mb-10 text-3xl font-bold">My Work</h3>
       <div>
         <ProjectCardList />
